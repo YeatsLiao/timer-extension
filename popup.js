4d2e23952aa202dae35e05794c6129289dcf926b
YeatsLiao <yeats_liao@outlook.com>
2024-05-30 13:44:31 +0800

    Add success and failure handling

--- popup.js
+++ popup.js
@@ -1,7 +1,17 @@
-const button = document.getElementById("confirm");
-button.addEventListener("click", function () {
+const confirmButton = document.getElementById("confirm");
+confirmButton.addEventListener("click", function () {
     const content = document.getElementById('content').value;
     const time = document.getElementById('time').value;
+
+    if (time === '') {
+        $('#errorModal').modal('show');
+        return;
+    }
+
+    // 显示模态对话框
+    document.getElementById('modalTime').innerText = time;
+    $('#successModal').modal('show');
+
     setTimeout(function () {
         chrome.notifications.create(
             '',
@@ -13,4 +23,19 @@ button.addEventListener("click", function () {
             }
         );
     }, Number(time) * 1000);
+
+
 });
+
+
+// 创建一个通用的关闭按钮处理函数
+function handleCloseButtonClick() {
+    $('#successModal').modal('hide');
+    $('#errorModal').modal('hide');
+}
+
+// 成功模态对话框的关闭按钮事件
+document.getElementById('successClose').addEventListener('click', handleCloseButtonClick);
+
+// 错误模态对话框的关闭按钮事件
+document.getElementById('errorClose').addEventListener('click', handleCloseButtonClick);
